ad90b207850e59ead70fc44ecea1c0ace8ecdaeb
Manikya Shama <253427819+manikya-ua@users.noreply.github.com>
2026-01-27 14:04:36 +0530

    Add drizzle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950 text-neutral-100`}
       >
         <ReactQueryProvider>
           <Navbar />
---
+++ src/database/drizzle/db.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DB Url required");
+}
+
+export const db = drizzle(process.env.DATABASE_URL);
